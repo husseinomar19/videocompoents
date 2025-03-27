b5132306e03dcf48725ee7499b9fae41b442ca3e
Hussein <omerhussen603@gmail.com>
2025-03-27 10:07:57 +0100

    Refactor video slider implementation and remove backup file

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
 document.addEventListener('DOMContentLoaded', function() {
     const sliderContainer = document.querySelector('.slider-container');
-    const slides = document.querySelectorAll('.slider-items');
     const prevBtn = document.querySelector('.arrow-left');
     const nextBtn = document.querySelector('.arrow-right');
-    let currentIndex = 2;
-    const totalSlides = slides.length;
-
     
+ 
+
+    const videos = [
+        { id: 1, url: "https://hyredStorage.s3.eu-central-003.backblazeb2.com/original_files/videos/allowed/admin-6752c7d53dc51.mp4" },
+        { id: 2, url: "https://hyredStorage.s3.eu-central-003.backblazeb2.com/original_files/videos/allowed/admin-67599fc6cf877.mp4" },
+        { id: 3, url: "https://hyredStorage.s3.eu-central-003.backblazeb2.com/original_files/videos/allowed/admin-66d58eebb8769.mp4" },
+        { id: 4, url: "https://hyredStorage.s3.eu-central-003.backblazeb2.com/original_files/videos/allowed/admin-6752cf62572d6.mp4" }
+    ];
+
+
+    sliderContainer.innerHTML = videos.map(video => `
+        <div class="slider-items">
+            <video src="${video.url}" controls></video>
+        </div>
+    `).join('');
+
+    const slides = document.querySelectorAll('.slider-items');
+    let currentIndex = 1;
+    const totalSlides = slides.length;
 
     function updateSlider() {
         // Reset all slides
@@ -48,8 +63,16 @@ document.addEventListener('DOMContentLoaded', function() {
     }
 
     // Initialize slider
+    
+    if(totalSlides == 1){
+        prevBtn.style.display = 'none';
+        nextBtn.style.display = 'none';
+        slides[0].classList.add('active');
 
+    }else{
         updateSlider();
+    }
+    //updateSlider();
 
     
 
